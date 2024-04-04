011b0d38c56a32d626d8480a02ccf9a34801ffd2
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2024-04-04 18:56:17 +0400

    Set webpack settings

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/js/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        watchFiles: './src/index.html',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            scriptLoading: 'defer',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
