e4e7ded33122b0b9ba70694bc651e0126997f3f8
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2024-04-09 17:01:40 +0400

    Remove FromList field from new task form

--- src/js/task.js
+++ src/js/task.js
@@ -1,10 +1,9 @@
 export default class Task {
-    constructor(name, description, dueDate, priority, fromTaskList) {
+    constructor(name, description, dueDate, priority, ID) {
         this.name = name;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
-        this.fromTaskList = fromTaskList;
         this.taskID = ID;
     }
 }
